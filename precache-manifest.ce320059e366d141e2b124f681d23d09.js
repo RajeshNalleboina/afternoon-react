f30f3317037553c5530e4ff67b46cb2329ca3d71
unknown <rajeshnalleboina94@gmail.com>
2020-08-02 20:43:49 +0530

    Updates

---
+++ precache-manifest.ce320059e366d141e2b124f681d23d09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16856a3139ea5381d40c6061db21f8a0",
+    "url": "/afternoon-react/index.html"
+  },
+  {
+    "revision": "b020c2e031c25a59415a",
+    "url": "/afternoon-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ce6e952a06a450b5dfa",
+    "url": "/afternoon-react/static/js/2.e390920d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/afternoon-react/static/js/2.e390920d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b020c2e031c25a59415a",
+    "url": "/afternoon-react/static/js/main.7426d52e.chunk.js"
+  },
+  {
+    "revision": "48e1fdd9cee00ce9202f",
+    "url": "/afternoon-react/static/js/runtime-main.5bbd83b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/afternoon-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2369d24d785f348feeb8b6cb8d555864",
+    "url": "/afternoon-react/static/media/man.2369d24d.svg"
+  }
+]);
